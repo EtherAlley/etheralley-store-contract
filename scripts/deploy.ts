95b9568e5b7cce2fa58d23b42eec1a7eae009325
Evan Fleming <evan.gordon.fleming@gmail.com>
2022-03-13 10:03:52 -0700

    adding basic ERC1155 implementation with OpenZeppelin

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Store = await ethers.getContractFactory("Store");
+  const store = await Store.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  await store.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Store deployed to:", store.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
